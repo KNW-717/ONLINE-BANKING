baa708addadd2339ce7f2077cac6cee39f4f12d8
Giovanni Antonio Gallina <118858179+KNW-717@users.noreply.github.com>
2026-06-18 10:11:55 +0000

    AGGIUNTO --> P2P con Ricerca Utente nel SIstema e Protezione da Pointer Aliasing

--- motore_bancario.c
+++ motore_bancario.c
@@ -29,3 +29,42 @@ bool effettuaPrelievo(Utente* u, double importo, Data dataSimulata) {
     printf("Prelievo di %.2lf EUR effettuato.\n", importo);
     return true;
 }
+
+// --- Cerca utente nel Sistema, Verifica Disponibilità, Effettua P2P e Aggiorna sald su conti utente ---
+bool effettuaP2P(Utente* mittente, ListaUtenti listaSistema, const char* usernameBeneficiario, double importo, Data dataSimulata) {
+    if (importo <= 0) return false;
+
+    // Ricerca Utente nel sistema
+    Utente* beneficiario = cercaUtentePerUsername(listaSistema, usernameBeneficiario);
+    if (beneficiario == NULL) {
+        printf("ERRORE: Utente '%s' inesistente.\n", usernameBeneficiario);
+        return false;
+    }
+    
+    // Pointer Aliasing --> Mittente deve essere diverso da Beneficiario
+    if (mittente == beneficiario) {
+        printf("OPERAZIONE NEGATA: Non puoi inviare un P2P a te stesso.\n");
+        return false;
+    }
+    
+    if (!verificaDisponibilita(mittente, importo)) 
+        return false;
+    // Aggiorna Saldi
+    mittente->saldo -= importo;
+    beneficiario->saldo += importo;
+    aggiungiTransazione(mittente, dataSimulata, "P2P_Inviato", usernameBeneficiario, importo);
+    aggiungiTransazione(beneficiario, dataSimulata, "P2P_Ricevuto", mittente->username, importo);
+
+    // Notifica per il mittente
+    char logTx[150];
+    sprintf(logTx, "Hai inviato %.2lf EUR a %s tramite P2P", importo, usernameBeneficiario);
+    pushNotifica(&(mittente->notifiche), logTx);
+
+    // Notifica per il destinatario
+    char logRx[150];
+    sprintf(logRx, "Hai ricevuto %.2lf EUR da %s tramite P2P", importo, mittente->username);
+    pushNotifica(&(beneficiario->notifiche), logRx);
+
+    printf("Trasferimento P2P completato.\n");
+    return true;
+}
