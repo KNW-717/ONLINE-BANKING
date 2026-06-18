852850e9ef354f14eeaf7cb8f4dde2162e6ca1e9
Giovanni Antonio Gallina <118858179+KNW-717@users.noreply.github.com>
2026-06-18 15:32:25 +0000

    AGGIUNTO --> Funzione di Previsione per n mesi, con: Saldo Virtuale stimato e controllo e rilevamento saldo in rosso

--- previsione.c
+++ previsione.c
@@ -29,3 +29,84 @@ bool dataMaggioreOUguale(Data d1, Data d2) {
     if (d1.mese != d2.mese) return d1.mese > d2.mese;
     return d1.giorno >= d2.giorno;
 }
+
+// ---Generazione Previsione per n mesi, con: saldo virtuale, data iterativa, controlli su saldo in rosso e rilevamento mese in cui saldo va in rosso, report finale ---
+void generaPrevisione(Utente* u, Data dataSimulataCorrente, int mesiPrevisione) {
+    double saldoVirtuale = u->saldo;
+    Data dataIterativa = dataSimulataCorrente;
+    Data dataFine = dataSimulataCorrente;
+    dataFine.mese += mesiPrevisione;
+    
+    // Check cambio anno
+    while (dataFine.mese > 12) 
+        {
+         dataFine.mese -= 12; 
+         dataFine.anno++; 
+        }
+
+    printf(ANSI_COLOR_CYAN "\n=== PREVISIONE A %d MESI ===\n" ANSI_COLOR_RESET, mesiPrevisione);
+
+    // Variabili per riportare se il conto è andato in rosso e dopo quanti mesi è successo 
+    bool primoRossoRilevato = false;
+    int mesiAlPrimoRosso = 0;
+
+    // Finché non arriva alla data prevista per la fine della simulazione
+    while (!(dataIterativa.anno == dataFine.anno && dataIterativa.mese == dataFine.mese && dataIterativa.giorno == dataFine.giorno)) {
+        Movimento* m = u->programmati;
+        while (m != NULL) {
+            //calcolo dei giorni presenti in ogni mese simulato
+            int giorniMeseCorrente = giorniInMese(dataIterativa.mese, dataIterativa.anno);
+            int giornoScadenza = (m->giornoRipetizione > giorniMeseCorrente) ? giorniMeseCorrente : m->giornoRipetizione;
+
+            // calcolo della differenza di mesi dalla data simulata alla data in cui è stato programmato il movimento
+            int deltaMesi = (dataIterativa.anno - m->dataInizio.anno) * 12 + (dataIterativa.mese - m->dataInizio.mese);
+
+            // verifica sulla data del movimento programmato, se siamo nel giorno e nel mese e con la periodicità corretta del movimento programmato, allora bisogna aggiornare il saldo Virtuale
+            if (dataMaggioreOUguale(dataIterativa, m->dataInizio) && dataIterativa.giorno == giornoScadenza && (deltaMesi % m->intervalloMesi == 0)) {
+                saldoVirtuale += m->importo;
+                if (m->importo >= 0) {
+                    printf("[%02d/%02d/%04d] Introito '%s' (" ANSI_COLOR_GREEN "+%.2lf EUR" ANSI_COLOR_RESET "). Saldo: %.2lf\n", dataIterativa.giorno, dataIterativa.mese, dataIterativa.anno, m->descrizione, m->importo, saldoVirtuale);
+                } 
+                else 
+                {
+                    printf("[%02d/%02d/%04d] Uscita '%s' (" ANSI_COLOR_RED "%.2lf EUR" ANSI_COLOR_RESET "). Saldo: %.2lf\n",
+                           dataIterativa.giorno, dataIterativa.mese, dataIterativa.anno, m->descrizione, m->importo, saldoVirtuale);
+
+                    //controllo saldo va in rosso
+                    if (saldoVirtuale < 0) {
+                        printf(ANSI_COLOR_RED "\n[!!!] COLLISIONE RILEVATA! Impossibile pagare '%s' [!!!]\n" ANSI_COLOR_RESET, m->descrizione);
+                        printf(ANSI_COLOR_YELLOW "Azione Consigliata: Spostare date o ricaricare almeno %.2lf EUR.\n\n" ANSI_COLOR_RESET, (saldoVirtuale * -1));
+                        if (!primoRossoRilevato) {
+                            //salvataggio dopo quanti mesi da inizio simulazione è stato rilevato il primo rosso
+                            mesiAlPrimoRosso = (dataIterativa.anno - dataSimulataCorrente.anno) * 12 + (dataIterativa.mese - dataSimulataCorrente.mese);
+                            primoRossoRilevato = true;
+                        }
+                    }
+                }
+            }
+            m = m->next; ///passaggio a successivo movimento programmato
+        }
+        avanzaGiorno(&dataIterativa); // finché data simulata != data fine simulazione
+    }
+
+    const char* coloreSaldo = (saldoVirtuale >= 0) ? ANSI_COLOR_GREEN : ANSI_COLOR_RED;
+
+    printf(ANSI_COLOR_CYAN "===================================================\n");
+    printf("=== SALDO FINALE STIMATO: %s%.2lf EUR" ANSI_COLOR_CYAN " ===\n" ANSI_COLOR_RESET, coloreSaldo, saldoVirtuale);
+
+    if (primoRossoRilevato) {
+        if (mesiAlPrimoRosso == 0) {
+            printf(ANSI_COLOR_YELLOW "[!] RESOCONTO: Il conto andra' in negativo gia' nel mese corrente di simulazione.\n" ANSI_COLOR_RESET);
+        } else {
+            printf(ANSI_COLOR_YELLOW "[!] RESOCONTO: Il conto andra' in negativo esattamente dopo %d mesi dall'inizio della simulazione.\n" ANSI_COLOR_RESET, mesiAlPrimoRosso);
+        }
+    } else {
+        printf(ANSI_COLOR_GREEN "[V] RESOCONTO: Il conto rimarra' stabile e in positivo per tutti i %d mesi simulati.\n" ANSI_COLOR_RESET, mesiPrevisione);
+    }
+    printf(ANSI_COLOR_CYAN "===================================================\n\n" ANSI_COLOR_RESET);
+}
+// .
+// . .
+// . . .
+// . . . . qui giacono le gocce di sudore versate per pensare,realizzare e, soprattutto, far funzionare questo pezzetto di codice . . . .
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
