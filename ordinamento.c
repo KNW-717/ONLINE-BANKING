f51eed52c9e66a0f12a137a0d442ac2e110518f9
Giovanni Antonio Gallina <118858179+KNW-717@users.noreply.github.com>
2026-06-18 13:35:55 +0000

    AGGIUNTO --> Ordina Storico e Ordina Utenti in base a 2 Criteri successivamente definiti nel main

--- ordinamento.c
+++ ordinamento.c
@@ -33,3 +33,63 @@ int comparaTimestamp(Timestamp t1, Timestamp t2) {
     if (t1.secondo != t2.secondo) return t1.secondo > t2.secondo ? 1 : -1;
     return 0;
 }
+
+// --- Ordina Storico in base al Criterio scelto nel main ---
+void ordinaStorico(Utente* u, int criterio) {
+    if (u->storico == NULL || u->storico->next == NULL) 
+        return;
+
+    bool scambiato = true; // ci serve per entrare nel while
+    Transazione* ptr1;
+    Transazione* lptr = NULL;
+
+    while (scambiato) {
+        scambiato = false;
+        ptr1 = u->storico;
+        while (ptr1->next != lptr) {
+            bool condizioneSwap = false;
+            int cmp = comparaTimestamp(ptr1->dataOraOperazione, ptr1->next->dataOraOperazione);
+            
+            // si prevede la scelta del criterio in base al numero selezionato nel main (2 criteri)
+            if (criterio == 1 && cmp > 0) condizioneSwap = true;
+            else if (criterio == 2 && cmp < 0) condizioneSwap = true;
+
+            // swap
+            if (condizioneSwap) {
+                scambiaTransazioni(ptr1, ptr1->next);
+                scambiato = true;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    }
+}
+
+// --- Ordina Utenti in base al Criterio scelto nel main ---
+void ordinaUtenti(ListaUtenti* lista, int criterio) {
+    if (*lista == NULL || (*lista)->next == NULL) return;
+
+    bool scambiato = true;
+    Utente* ptr1;
+    Utente* lptr = NULL;
+
+    while (scambiato) {
+        scambiato = false;
+        ptr1 = *lista;
+        while (ptr1->next != lptr) {
+            bool condizioneSwap = false;
+            
+            // si prevede la scelta del criterio in base al numero selezionato nel main (2 criteri)
+            if (criterio == 1 && ptr1->saldo < ptr1->next->saldo) condizioneSwap = true;
+            else if (criterio == 2 && ptr1->saldo > ptr1->next->saldo) condizioneSwap = true;
+
+            // swap
+            if (condizioneSwap) {
+                scambiaUtenti(ptr1, ptr1->next);
+                scambiato = true;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    }
+}
