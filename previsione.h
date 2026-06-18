25d0c38a3a62cc8908749701b8483cb3f541b582
Giovanni Antonio Gallina <118858179+KNW-717@users.noreply.github.com>
2026-06-18 14:44:43 +0000

    AGGIUNTO --> Funzione Genera Previsione da Implementare in previsione.c

---
+++ previsione.h
@@ -0,0 +1,8 @@
+// Responsabilità:Motore asincrono per la previsione finanziaria.
+#ifndef PREVISIONE_H_INCLUDED
+#define PREVISIONE_H_INCLUDED
+#include "strutture.h"
+
+void generaPrevisione(Utente* u, Data dataSimulataCorrente, int mesiPrevisione);
+
+#endif // PREVISIONE_H_INCLUDED
