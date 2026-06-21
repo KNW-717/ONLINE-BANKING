ce300757341c5b793b16768a3f127d0490102747
Giovanni Antonio Gallina <118858179+KNW-717@users.noreply.github.com>
2026-06-21 21:12:05 +0000

    AGGIUNTO --> Funzioni di Salvataggio e Esplora Backup Binario

--- io_file.h
+++ io_file.h
@@ -6,4 +6,6 @@
 void salvaDatabase(ListaUtenti lista, const char* nomeFile);
 ListaUtenti caricaDatabase(const char* nomeFile);
 
+void salvaBackupBinario(ListaUtenti lista, const char* nomeFileBin);
+void esploraBackupBinarioConFseek(const char* nomeFileBin, int indiceBersaglio);
 #endif // IO_FILE_H_INCLUDED
